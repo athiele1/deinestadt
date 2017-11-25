ef85c5d01f7ee48931ec73daeaf14d26e6782281
athiele <athiele@uni-bremen.de>
2017-11-25 17:29:53 +0100

    struktur 2

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-function addTopMenu() {
+/**
+main.js
+
+Eine externe und zentrale Javascriptdatei, wo alle anderen Seiten
+drauf zugreifen können sobald
+"<script src="main.js"></script>" 
+im Head hinzugefügt wurde.
+
+**/
+
+
+
+function addHeadMenu() {
+	
+	//<div align="center" id="mitte"></div>
     var header = new Image();
+	var container = document.createElement("div");
+	//container.align = "center";
+	
+	
 	header.src = "pics/header.png";
-		
-	document.body.appendChild(header);
 	
-	
+	container.appendChild(header);
+	document.body.appendChild(container);
 	
 }

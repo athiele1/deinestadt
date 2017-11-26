c39eebca5460a0e1730e29e00876069297de4b30
athiele <athiele@uni-bremen.de>
2017-11-26 17:11:09 +0100

    added 3 buttons

--- main.js
+++ main.js
@@ -14,13 +14,35 @@ function addHeadMenu() {
 	
 	//<div align="center" id="mitte"></div>
     var header = new Image();
+	header.src = "pics/header.png";
+	
 	var container = document.createElement("div");
 	//container.align = "center";
 	
 	
-	header.src = "pics/header.png";
+	var b1 = new Image();
+	var b2 = new Image();
+	var b3 = new Image();
+	var spacer = new Image();
+	
+	b1.src = "pics/b_initiative.jpg";
+	b2.src = "pics/b_finder.jpg";
+	b3.src = "pics/b_partner.jpg";
+	spacer.src = "pics/b_spacer.jpg";
+	
+	
 	
 	container.appendChild(header);
-	document.body.appendChild(container);
 	
+	container.appendChild(spacer);
+	container.appendChild(b1);
+	container.appendChild(spacer.cloneNode(true));
+	container.appendChild(spacer.cloneNode(true));
+	container.appendChild(b2);
+	container.appendChild(spacer.cloneNode(true));
+	container.appendChild(spacer.cloneNode(true));
+	container.appendChild(b3);
+	container.appendChild(spacer.cloneNode(true));
+	
+	document.body.appendChild(container);	
 }

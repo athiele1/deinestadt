0112555041dc7b059b8c9484dea5e5da27c4038c
weingrtn <janis.weingaertner@web.de>
2018-01-17 15:59:26 +0100

    footer changes

--- main.js
+++ main.js
@@ -26,190 +26,190 @@ var btns = [];
 
 //Füllt den oberen Kopfbereich
 function addHeadMenu(container) {
-	
-	
+
+
     /*var header = new Image();
 	header.src = "pics/logo_200.png";*/
-	//var headerDiv = document.createElement("div");
-	
-	//headerDiv.id = "div100";
-	//headerDiv.appendChild(header);
-	
-	//container.appendChild(headerDiv);
-	
-	
-	//var container = document.createElement("div");
-	////container.align = "center";
-	
-	
-	var divUpMenu = document.createElement("div");
-	//divUpMenu.style.width = "1242px";
-	divUpMenu.style.height = "50px";
-	divUpMenu.style.borderTop = "1px solid #72e6ab";
+    //var headerDiv = document.createElement("div");
+
+    //headerDiv.id = "div100";
+    //headerDiv.appendChild(header);
+
+    //container.appendChild(headerDiv);
+
+
+    //var container = document.createElement("div");
+    ////container.align = "center";
+
+
+    var divUpMenu = document.createElement("div");
+    //divUpMenu.style.width = "1242px";
+    divUpMenu.style.height = "50px";
+    divUpMenu.style.borderTop = "1px solid #72e6ab";
     divUpMenu.style.borderBottom = "1px solid #72e6ab";
-	divUpMenu.style.backgroundColor = "white";
-    
-	//divUpMenu.style.boxShadow =  "0px 10px 10px grey";
-	
-	var divHolder = document.createElement("div");
-	//divHolder.style.border = "1px solid red";
-	
-	divHolder.style.width = "1050px";
-	divHolder.style.margin = "0 auto";
-	divHolder.style.marginTop = "4px";
-	divHolder.style.display = "block";
-	
-	for(var i = 0; i < 6; i++){
-		
-		var btni = document.createElement("input");
-		btni.type = "button";
-		btni.value = buttonText[i];
-		btni.classList.add("topButton");
-		btni.id = "" + i;
-		
-		btni.addEventListener("click", function(){
-			window.location = buttonLink[this.id];
-		
-		});
-		
-		
-		
-		
-		/*
+    divUpMenu.style.backgroundColor = "white";
+
+    //divUpMenu.style.boxShadow =  "0px 10px 10px grey";
+
+    var divHolder = document.createElement("div");
+    //divHolder.style.border = "1px solid red";
+
+    divHolder.style.width = "1050px";
+    divHolder.style.margin = "0 auto";
+    divHolder.style.marginTop = "4px";
+    divHolder.style.display = "block";
+
+    for(var i = 0; i < 6; i++){
+
+        var btni = document.createElement("input");
+        btni.type = "button";
+        btni.value = buttonText[i];
+        btni.classList.add("topButton");
+        btni.id = "" + i;
+
+        btni.addEventListener("click", function(){
+            window.location = buttonLink[this.id];
+
+        });
+
+
+
+
+        /*
 		var span1 = document.createElement("span");
 		span1.style.width = "200px";
 		span1.style.height = "20px";
 		span1.style.display = "inline-block";
 		span1.style.position = "relative";
 		span1.style.marginTop = "10px"
-		
+
 		var span2 = document.createElement("span");
 		span2.style.height = "30px";
 		span2.style.display = "table";
 		span2.style.margin = "0 auto";
-		
-		
+
+
 		span2.innerHTML = buttonText[i];
-		
+
 		span2.classList.add("noselect");
-		
-		
+
+
 		span2.style.border = "5px solid white";
 		//span2.style.background = "white";
 		//span2.style.userSelect = "none";
-		
+
 		//span2.style.position = "relative";
-		
+
 		//var bi = document.createTextNode(buttonText[i]);
 		//bi.textAlign = "center";
-		
+
 		//span1.style.border = "thick solid #0000FF";
-		
-		
-		
-		
+
+
+
+
 		if(i != 2){
 			span1.style.borderRight = "1px solid grey";
-			
+
 		}		
-		
+
 		span2.id = "" + i;
 		span2.addEventListener("mouseover", function(){
 			alert("§l")
 			//this.style.borderBottom = "5px solid red";
 			selectButton(this);
 		})
-		
+
 		span2.addEventListener("mouseleave", function(){
-			
+
 			deselectButton(this);
-			
+
 		})
-		
-		
-		
+
+
+
 		//span2.style.border = "thick solid #FF0000";
 		//divi2.style.border = "thick solid #000000";
-		
-		
+
+
 		//bi.childNodes[0].style.color = "red";
 		//alert(bi.data);
-		
+
 		//buttons2.push(bi);
 		//span2.appendChild(bi);
 		span1.appendChild(span2);
 		//divi2.appendChild(bi);
-		
+
 		divUpMenu.appendChild(span1);
 		*/
-		
-		btns.push(btni);
-		divHolder.appendChild(btni);
-		
-		if(i < 5){
-		 var lineI = document.createElement("span");
-		 //lineI.style.width = "2px";
-		 //lineI.innerHTML = "&nbsp";
-		 //lineI.style.background = "grey";
-		 lineI.style.borderLeft = "2px solid grey";
-		  divHolder.appendChild(lineI);
-		}
-		
-		
-	}
-	
-	/*var fb = new Image();
+
+        btns.push(btni);
+        divHolder.appendChild(btni);
+
+        if(i < 5){
+            var lineI = document.createElement("span");
+            //lineI.style.width = "2px";
+            //lineI.innerHTML = "&nbsp";
+            //lineI.style.background = "grey";
+            lineI.style.borderLeft = "2px solid grey";
+            divHolder.appendChild(lineI);
+        }
+
+
+    }
+
+    /*var fb = new Image();
 	fb.src = "pics/facebook.png";
 	fb.style.display = "inline";
 	fb.style.width = "30px";
 	fb.style.height = "30px";*/
-	
-	divUpMenu.appendChild(divHolder);
-	
-	//divUpMenu.appendChild(fb);
-	
-	container.append(divUpMenu);
-	
-	/*container.append(document.createElement("br"));
-	
-	
-	
+
+    divUpMenu.appendChild(divHolder);
+
+    //divUpMenu.appendChild(fb);
+
+    container.append(divUpMenu);
+
+    /*container.append(document.createElement("br"));
+
+
+
 	var spacer = new Image();
-	
+
 	b1.type = "image";
 	b2.type = "image";
 	b3.type = "image";
-	
+
 	resetButtons(); //Setzt das default image
 
-	
+
 	spacer.src = "pics/b_spacer.jpg";
-	
-	
+
+
 	//Macht die "Buttons" anklickbar
-	
+
 	b1.addEventListener("click", function(){
 		window.location="index.html";
-		
+
 	});
-	
+
 	b2.addEventListener("click", function(){
 		window.location="finder.html";
-		
+
 	});
-	
+
 	b3.addEventListener("click", function(){
 		window.location="partner.html";
-		
+
 	});
-	
-	
-	
-	
+
+
+
+
 	//Fügt die Elemente in der richtigen Reihenfolge mit "Spacern" dazwischen ein.
-	
-	
-	
+
+
+
 	container.appendChild(spacer);
 	container.appendChild(spacer.cloneNode(true));
 	container.appendChild(spacer.cloneNode(true));
@@ -226,57 +226,47 @@ function addHeadMenu(container) {
 	container.appendChild(spacer.cloneNode(true));
 	container.appendChild(spacer.cloneNode(true));
 	*/
-	//document.body.appendChild(container);	
+    //document.body.appendChild(container);	
 }
 
 //Füllt den unteren (Impressum etc) Bereich
 function addButtomMenu(container2) {
-	
-	var horizontalLine = document.createElement("hr");
+    
+    var footer = document.createElement("footer");
+    footer.style.position = "relative";
+    footer.id = "bottom-footer"
 
-	container2.appendChild(horizontalLine);
-	
-	
-	var divMenu = document.createElement("footer") ;
-	divMenu.id = "divBottomMenu";
-    divMenu.style.position = "fixed";
-    divMenu.style.bottom = "0";
-	
-	 
-	var h4 = document.createElement("h4");
-	
-	//var textMenu = document.createTextNode("Impressum");	
+    //var textMenu = document.createTextNode("Impressum");	
 
-	var impressum = document.createElement("p");
-	impressum.innerHTML = '<a href="impressum.html" title="Impressum">Impressum</a>';
-	
-	//var contact = document.createElement("p");
-	//contact.innerHTML = '<a href="contact.html" title="Kontakt">Kontakt</a>';
+    var impressum = document.createElement("p");
+    impressum.innerHTML = '<a href="impressum.html" title="Impressum">Impressum</a>';
 
-	impressum.innerHTML += '&nbsp;&nbsp;&nbsp<a href="contact.html" title="Kontakt">Kontakt</a>';
+    var contact = document.createElement("p");
+    contact.innerHTML = '<a href="contact.html" title="Kontakt">Kontakt</a>';
 
-	divMenu.appendChild(h4);
-	h4.appendChild(impressum);
-	
-	container2.appendChild(divMenu);
-	
-	
-	
+    //var contact = document.createElement("p");
+    //contact.innerHTML = '<a href="contact.html" title="Kontakt">Kontakt</a>';
+    
+    footer.appendChild(impressum);
+    footer.appendChild(contact);
+    container2.appendChild(footer);
+
+
 }
 
 //Deselectiert alle oberen Menüknöpfe
 function resetButtons(){
-	for(var btni in btns){
-		btni.className = "topButton";		
-	}	
+    for(var btni in btns){
+        btni.className = "topButton";		
+    }	
 }
 
 //Selectiert einen oberen Menüknöpfe
 function markButton(i){
-	resetButtons();
-	btns[i].className = "topButtonClicked";
-	//btns[i].classList.add("topButtonClicked");
-	
+    resetButtons();
+    btns[i].className = "topButtonClicked";
+    //btns[i].classList.add("topButtonClicked");
+
 }
 
 /*function selectButton(spani){
@@ -291,7 +281,7 @@ function deselectButton(spani){
 	if(spani.id == "none"){
 		spani = spani.parentNode;	
 	}
-	
+
 	spani.style.borderBottom = "0px";
 	//alert(buttons2[i].data);
 	spani.innerHTML = buttonText[spani.id].fontcolor("black");
